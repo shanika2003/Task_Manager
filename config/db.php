38cadce3bcbc9628d2e39d54f737a05a514e051e
shanika harshani <hshanika987@gmail.com>
2025-07-23 11:37:04 +0530

    initialize project,Dataaccess layer

---
+++ config/db.php
@@ -0,0 +1,25 @@
+<?php
+// Database configurations 
+class Database{
+    private $host = "localhost";
+    private $db_name = "task_manager";
+    private $user = "root";
+    private $password = "2003";
+    public $conn;
+
+    public function getConnection(){
+        $this -> conn = null;
+        try{
+            $this -> conn = new PDO(
+                "mysql:host".$this->host. ";dbname=".$this->db_name,
+                $this->user,
+                $this->password
+            );
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $e){
+            echo "Connection Error:".$e->getMessage();
+        }
+        return $this->conn;
+    }
+}
+
