c08ee495cfd00cd2f312a951e59976076303fea3
shanika2003 <hshanika987@gmail.com>
2025-07-23 12:11:55 +0530

    header complete

---
+++ presentation/partials/footer.php
@@ -0,0 +1,13 @@
+<header class="bg-primary text-white py-4 mb-4 shadow">
+    <div class="container">
+        <div class="d-flex justify-content align-items-">
+            <h1 class="h3 mb-0">
+                <i class="fas fa-tasks me-2"></i></i>Task Manager
+            </h1>
+            <a href="" class="btn btn-light">
+                <i class="fas far-plus me-1"></i>New Task
+            </a>
+        </div>
+    </div>
+
+</header>
